aeacc109d06721f6408ec84d5ecda9396ffc3e1c
Mihil Jose <46615662+mihiljose@users.noreply.github.com>
2021-04-24 09:34:52 +0530

    create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while at_goal()!=True: 
+    while front_is_clear()==True:
+        move()
+
+    while wall_in_front()==True:
+        jump()
